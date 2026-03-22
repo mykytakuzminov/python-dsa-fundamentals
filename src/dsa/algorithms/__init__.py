d52913715431e158ac2896814a90b03210db6645
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-03-22 15:03:09 +0100

    feat: configure public API via __init__.py exports

---
+++ src/dsa/algorithms/__init__.py
@@ -0,0 +1,33 @@
+from .recursion import (
+    factorial,
+    fibonacci,
+    recursive_max,
+    recursive_reverse,
+    recursive_sum,
+    sum_nested_list,
+)
+from .searching import binary_search
+from .sorting import (
+    bubble_sort,
+    counting_sort,
+    insertion_sort,
+    merge_sort,
+    quick_sort,
+    selection_sort,
+)
+
+__all__ = [
+    "binary_search",
+    "bubble_sort",
+    "counting_sort",
+    "factorial",
+    "fibonacci",
+    "insertion_sort",
+    "merge_sort",
+    "quick_sort",
+    "recursive_max",
+    "recursive_reverse",
+    "recursive_sum",
+    "selection_sort",
+    "sum_nested_list",
+]
---
+++ src/dsa/data_structures/__init__.py
@@ -0,0 +1,19 @@
+from .binary_search_tree import BinarySearchTree
+from .doubly_linked_list import DoublyLinkedList
+from .graph import Graph
+from .hash_map import HashMap
+from .heap import Heap
+from .queue import Queue
+from .singly_linked_list import SinglyLinkedList
+from .stack import Stack
+
+__all__ = [
+    "BinarySearchTree",
+    "DoublyLinkedList",
+    "Graph",
+    "HashMap",
+    "Heap",
+    "Queue",
+    "SinglyLinkedList",
+    "Stack",
+]
